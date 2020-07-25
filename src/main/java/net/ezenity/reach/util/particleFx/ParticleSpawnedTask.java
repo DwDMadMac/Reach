8ddc49a98b064a98aad17074fcd75c82ad1f2930
ezenity <anthonymmacallister@gmail.com>
2020-07-24 22:01:59 -0400

    Particle Task.
    To keep the particle running at a rate that will not crash the client or create lag, I decided to try executors method. This will still need some testing done on a larger scale, however from what I have tested this method seems to work okay. This will allow you to set a particle task to any player with a given particle design, cooldown timer, and particle spawn timer. Will be adding more constructors with other options to make it idea for different scenarios. Will also make other constructors with less options with default values.

--- src/main/java/net/ezenity/reach/util/particleFx/ParticleSpawnedTask.java
+++ src/main/java/net/ezenity/reach/util/particleFx/ParticleSpawnedTask.java
@@ -12,27 +12,65 @@ import java.util.concurrent.TimeUnit;
 
 import static net.ezenity.reach.util.CountdownFx.*;
 
+/**
+ * This class is used for custom particle spawning. Each particle is spawned as a executor which will be used
+ * asynchronously and in the event the same particle is being spawned for the given hash code, this particle
+ * task will not execute and continue to its pre-requites code.
+ */
 public class ParticleSpawnedTask {
-    private static final ScheduledExecutorService scheduled = Executors.newScheduledThreadPool(1);
-    private static ScheduledFuture<?> particleHandler;
+    /**
+     * This field we are using an Executor Service as a higher level replacement for working with threads directly. In this
+     * case we are capable of running asynchronously tasks and manage a pool of threads. This will allow us to stop the
+     * creation of new threads manually. All threads of the internal pool will be reused under the hood for revenant task.
+     * This allows the particle task to run as many concurrent tasks as we want throughout the life-cycle of the application
+     * with a single executor service. We are using {@link Executors#newScheduledThreadPool(int)} to create an executor
+     * service backed by a thread-pool of size one. If we need to increase the pool size, we can by simply passing a larger
+     * value than one.
+     */
+    private static final ScheduledExecutorService THREAD_EXECUTOR = Executors.newScheduledThreadPool(1);
+    /**
+     * We use a {@link ScheduledFuture} to represent the results of our asynchronous delayed particle computation.
+     */
+    private static ScheduledFuture<?> PARTICLE_HANDLER;
 
-    public static void setParticleTask(Player player, Particle particle, String particleDesign) {
-        if (countdownSecondsRemaining(player.getUniqueId(), particleDesign) == 0)
-            removeCountdowns(player.getUniqueId(), particleDesign);
-
-        if (!countdownActive(player.getUniqueId(), particleDesign)) {
-            Logger.info("Countdown no longer activated, create particle");
+    /**
+     * We will use this method to set our given particle to the plausible demand. The particle will be set using an
+     * executor asynchronously and will run until the particle spawn rate has ended. Once the timer hits it limit the
+     * executor will then cancel and shutdown. Before spawning the particle, this method will check against a hashmap
+     * to ensure that the currently given hash code is not running. If there is still a particle in progress or if
+     * there is a cooldown(countdown) the task will not spawn and if {@link Logger#info(String)} is enabled, will output the
+     * remaining time length of the countdown.
+     *
+     * @param player get player
+     * @param particle get particle
+     * @param particleDesign get particle design
+     * @param cooldown get cooldown in seconds
+     * @param particleSpawnedTimer get particle spawn rate in seconds
+     */
+    public static void setParticleTask(Player player, Particle particle, String particleDesign, int cooldown, int particleSpawnedTimer) {
+        if (!(System.currentTimeMillis() > getCountdown(player.getUniqueId(), particleDesign))) {
+            Logger.info("Count Time  " + getCountdown(player.getUniqueId(), particleDesign));
+            Logger.info("System Time " + System.currentTimeMillis());
+        } else {
             Particles customParticle = new Particles(player, particle, particleDesign);
-            particleHandler = scheduled.scheduleAtFixedRate(customParticle, 0, 250, TimeUnit.MILLISECONDS);
-            scheduled.schedule(() -> {
-                particleHandler.cancel(true);
-            }, 1, TimeUnit.SECONDS);
+            PARTICLE_HANDLER = THREAD_EXECUTOR.scheduleAtFixedRate(customParticle, 0, 250, TimeUnit.MILLISECONDS);
+            THREAD_EXECUTOR.schedule(() -> {
+                PARTICLE_HANDLER.cancel(true);
+            }, particleSpawnedTimer, TimeUnit.SECONDS);
+            Logger.info("==============================================================");
+            Logger.info("==============================================================");
+            Logger.info("Countdown no longer activated, create particle");
             Logger.info("&1Create countdown");
-            setCountdown(player.getUniqueId(), particleDesign, 10);
-        }
+            setCountdown(player.getUniqueId(), particleDesign, cooldown);
+            Logger.info("==============================================================");
+            Logger.info("Get countdown in milliseconds " + getCountdown(player.getUniqueId(), particleDesign));
+            Logger.info("System current time in milliseconds " + System.currentTimeMillis());
+            Logger.info("==============================================================");
+            Lang.send(player, "Countdown activated for " + particleDesign);
+            Logger.info("==============================================================");
 
-        if (particleHandler.isCancelled())
-            scheduled.shutdown();
-        Lang.send(player, "Countdown activated for " + particleDesign);
+            if (PARTICLE_HANDLER.isCancelled())
+                THREAD_EXECUTOR.shutdown();
+        }
     }
 }
