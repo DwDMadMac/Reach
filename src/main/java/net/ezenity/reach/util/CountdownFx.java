f868892a6a471ecd73b4a93bc070788fede66403
ezenity <anthonymmacallister@gmail.com>
2020-07-24 22:07:17 -0400

    Countdown framework.
    The cooldown framework I was using from aadk was not working to fit my needs. Also found some issues with performance. minor however. I decided to create my own framework using a little bit from aadk "very simply cooldown library" with some things I found useful. This is where creating the CustomIndex class came in use. This allowed me to have two string keys inside a map and create my custom hashmap will still being able to look it up with ease.

--- src/main/java/net/ezenity/reach/util/CountdownFx.java
+++ src/main/java/net/ezenity/reach/util/CountdownFx.java
@@ -1,8 +1,9 @@
 package net.ezenity.reach.util;
 
+import net.ezenity.reach.util.countdownFx.CustomIndex;
+import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -10,115 +11,75 @@ import java.util.UUID;
 /**
  * A very simple countdown library
  *
- * @author aadnk
+ * TODO: NEEDED Methods Below
+     - public Map<UUID, Long> getAllTimers();
+     - public Map<String, Long> getSpecificTimer();
+     - public boolean activatedTimers();
+     - public void removeTimer();
+     - public void removeSpecificTimer();
+ *
+ * @author anthonymmacallister
  */
 public class CountdownFx {
-    private static final Map<UUID, Map<String, Long>> COUNTDOWNS = new HashMap<>();
+    /**
+     * Countdown map. Create a hashmap using two string objects from our CustomIndex class.
+     */
+    private static final Map<CustomIndex, Long> COUNTDOWNS = new HashMap<>();
 
     /**
-     * Update a countdown for the specified player.
+     * We use this method to declare our custom unique String Objects which will be
+     * return as an integer type value, representing the returned index. This method
+     * will allow you to input two String objects for a map key using the same value.
      *
      * @param playerID get player uuid
-     * @param countdownKey custom countdown key
-     * @param delay - number of seconds until the countdown will expire again.
-     * @return The previous number of milliseconds until expiration.
+     * @param countdownKey get custom countdown key
+     * @return hash code value for the two given string objects
      */
-    public static void setCountdown(UUID playerID, String countdownKey, long delay) {
-//        return calculateRemainder(
-//                countdowns.put(
-//                    player.getUniqueId(),
-//                    countdownKey,
-//                    /*secToMilli(delay)*/System.currentTimeMillis() + delay
-//                ));
-
-//        return calculateRemainder(COUNTDOWNS.computeIfAbsent(playerID, k -> new HashMap<>())
-//                .put(countdownKey, delay));
-        COUNTDOWNS.computeIfAbsent(playerID, k -> new HashMap<>())
-                .put(countdownKey, System.currentTimeMillis() + (delay * 1000));
+    private static CustomIndex customIndexHash(UUID playerID, String countdownKey){
+        return new CustomIndex(playerID.toString(), countdownKey);
     }
 
     /**
-     * Retrieve all the countdowns that are associated with the players UUID and
-     * the given countdown key.
+     * Set the specific countdown to a players UUID. This method will also use a specific
+     * countdown key to identify the time left of the countdown. Both playerID and countdownKey
+     * are keyed in to create a unique hashCode. The delay is inputted as seconds for
+     * configuration purposes on end-user. Use a simply conversion to change the integer
+     * into a long for milliseconds. The converted seconds is then added to the current system
+     * time in milliseconds for easy mapping.
      *
-     * @param countdownKey - countdown key to locate
-     * @return all the counts that are associated with the countdown key
+     * @param playerID get player uuid
+     * @param countdownKey get custom countdown key
+     * @param delay number of seconds until the countdown will expire
      */
-    public static Map<UUID, Long> getKeyCountdowns(String countdownKey) {
-        Map<UUID, Long> allKeyCountdowns = new HashMap<>();
-        COUNTDOWNS.forEach((key, value) -> {
-            Long expire = value.get(countdownKey);
-            if (expire != null)
-                allKeyCountdowns.put(key, expire);
-        });
-        return Collections.unmodifiableMap(allKeyCountdowns);
+    public static void setCountdown(UUID playerID, String countdownKey, int delay){
+        long delayConvert = (long) delay * 1000;
+        COUNTDOWNS.put(customIndexHash(playerID, countdownKey), delayConvert + System.currentTimeMillis());
     }
 
     /**
-     * Determine if a given countdown has expired. If it has not expired, countdown will
-     *  return TRUE.
+     * This method will determine if a countdown is still active. If the countdown is no longer active
+     * the method will return false. If the countdown is still active, the method will return true.
      *
      * @param playerID get player uuid
-     * @param countdownKey custom countdown key
+     * @param countdownKey get custom countdown key
      * @return TRUE if the countdown is still active, FALSE otherwise.
      */
     public static boolean countdownActive(UUID playerID, String countdownKey) {
-        Map<String, Long> countdowns = COUNTDOWNS.get(playerID);
-        return countdowns != null && countdowns.containsKey(countdownKey);
+        return System.currentTimeMillis() >= getCountdown(playerID, countdownKey);
     }
 
     /**
-     * Get the remaining seconds left for the specific countdown key that is
-     * associated with the players uuid.
+     * This will get the added system milliseconds for the given hashCode of the playerID and
+     * countdown key. If the system time in milliseconds have passed, it will return 0L instead.
+     * This method is used to compute for active countdowns. For an example of how to use this
+     * method, refer to:
+     * {@link net.ezenity.reach.util.particleFx.ParticleSpawnedTask#setParticleTask(Player, Particle, String, int, int)}
      *
      * @param playerID get player uuid
-     * @param countdownKey custom countdown key
-     * @return if countdown is null return 0, else return seconds left for countdown
-     */
-    public static int countdownSecondsRemaining(UUID playerID, String countdownKey){
-        Map<String, Long> countdowns = COUNTDOWNS.get(playerID);
-        if (countdowns == null)
-            return 0;
-        Long expireTime = countdowns.get(countdownKey);
-        if (expireTime == null)
-            return 0;
-        return (int) ((expireTime - System.currentTimeMillis()) / 1000);
-    }
-
-    /**
-     * Remove the given countdown key associated with the players uuid.
-     *
-     * @param playerID player uuid
-     * @param countdownKey custom countdown key
-     */
-    public static void removeCountdowns(UUID playerID, String countdownKey) {
-        Map<String, Long> countdowns = COUNTDOWNS.get(playerID);
-        if (countdowns != null)
-            countdowns.remove(countdownKey);
-    }
-
-    /**
-     * Remove a specific countdown associated to a specific player.
-     * 
-     * @param player get given player
      * @param countdownKey get custom countdown key
+     * @return the long value associated with the index hash
      */
-    public static void removeSpecificCountdown(Player player, String countdownKey) {
-        COUNTDOWNS.remove(player.getUniqueId(), countdownKey);
-    }
-
-    private static long secToMilli(int seconds) {
-        return seconds * 1000;
-    }
-
-    /**
-     * Calculate the time left until it is due to expire. Once expired it will fall back to
-     * {@link Long#MIN_VALUE}.
-     *
-     * @param expireTime expire time in milliseconds
-     * @return time left in milliseconds, if not null
-     */
-    private static long calculateRemainder(Long expireTime) {
-        return expireTime != null ? expireTime - System.currentTimeMillis() : Long.MIN_VALUE;
+    public static long getCountdown(UUID playerID, String countdownKey) {
+        return COUNTDOWNS.getOrDefault(customIndexHash(playerID, countdownKey), 0L);
     }
 }
