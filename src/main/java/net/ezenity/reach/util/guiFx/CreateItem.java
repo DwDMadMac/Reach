a7c95b430ca90da0e3d1cf51c629d96027766942
ezenity <anthonymmacallister@gmail.com>
2020-08-08 14:55:13 -0400

    JavaDoc & Param reformatting.

--- src/main/java/net/ezenity/reach/util/guiFx/CreateItem.java
+++ src/main/java/net/ezenity/reach/util/guiFx/CreateItem.java
@@ -4,6 +4,9 @@ import fr.minuskube.inv.content.InventoryContents;
 import net.ezenity.reach.configuration.Config;
 import net.ezenity.reach.Main;
 import net.ezenity.reach.configuration.Lang;
+import net.ezenity.reach.util.guiFx.providers.MainProvider;
+import net.ezenity.reach.util.guiFx.providers.ToolsProvider;
+import net.ezenity.reach.util.guiFx.providers.TreeSpawnerProvider;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -20,29 +23,28 @@ public abstract class CreateItem extends CustomItemStack {
     private final Main plugin = Main.getInstance();
 
     /**
-     * Get a custom item from the configuration file. Just add in the item name exactly how it is
-     * inside the config.yml {@link Config} without the suffix declaration, when calling the
-     * getItem() method. Usage can be viewed {@link MainProvider#init(Player, InventoryContents)}
+     * Get a custom item from the configuration file.
      * <p>
-     * Suffix Declarations:
+     * Just add in the item location exactly how it is inside the config.yml ({@link Config})
+     * without the suffix declaration, when calling the getConfigItem() method. Usage can
+     * be viewed in our "Provider" classes:
      * <ul>
-     *    <li>enabled</li>
-     *    <li>type</li>
-     *    <li>title</li>
-     *    <li>lore</li>
+     *     <li>{@link MainProvider}</li>
+     *     <li>{@link ToolsProvider}</li>
+     *     <li>{@link TreeSpawnerProvider}</li>
      * </ul>
      *
-     * @param itemString Gets a string. Can use this to get a String from a config file.
+     * @param configItemLocation gets the string location from the configuration file.
      * @return custom tool with prerequisites from the config file
      */
     @Override
-    public ItemStack getItem(String itemString) {
-        String itemType = (String) plugin.getConfig().get("portal." + itemString + ".type");
+    public ItemStack getConfigItem(String configItemLocation) {
+        String itemType = (String) plugin.getConfig().get("portal." + configItemLocation + ".type");
         ItemStack itemStack = new ItemStack(Objects.requireNonNull(Material.getMaterial(Objects.requireNonNull(itemType))));
         ItemMeta itemMeta = itemStack.getItemMeta();
-        Objects.requireNonNull(itemMeta).setDisplayName(Lang.colorize(plugin.getConfig().getString("portal." + itemString + ".title")));
+        Objects.requireNonNull(itemMeta).setDisplayName(Lang.colorize(plugin.getConfig().getString("portal." + configItemLocation + ".title")));
         itemMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-//        itemMeta.setCustomModelData(itemIdentifier);
+//        itemMeta.setCustomModelData();
 //        if (this.enchantment != null)
 //            itemMeta.addEnchant(enchantment, enchantLevel, levelRestrict); // TODO: Make method for multiple enchantment usages
 //        if (this.itemFlag != null)
@@ -50,7 +52,7 @@ public abstract class CreateItem extends CustomItemStack {
 //        if (this.)
 //            itemMeta.addAttributeModifier(Attribute.GENERIC_ARMOR, ); // TODO: Make method for a system of player buffs & debuffs (check Wiki)
         List<String> itemLore = new ArrayList<>();
-        for (String newLines : plugin.getConfig().getStringList("portal." + itemString + ".lore")) {
+        for (String newLines : plugin.getConfig().getStringList("portal." + configItemLocation + ".lore")) {
             itemLore.add(ChatColor.translateAlternateColorCodes('&', newLines));
         }
         itemMeta.setLore(itemLore);
