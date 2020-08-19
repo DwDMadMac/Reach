d1ff99803da10c2d5b4d9f9542695af2b0973c1a
ezenity <anthonymmacallister@gmail.com>
2020-08-18 23:47:27 -0400

    JavaDoc Update.

--- src/main/java/net/ezenity/reach/configuration/Lang.java
+++ src/main/java/net/ezenity/reach/configuration/Lang.java
@@ -15,13 +15,17 @@ import org.bukkit.configuration.file.YamlConfiguration;
  * Here you will find all the default language options
  */
 public class Lang {
+    /**
+     * This is the header for the language file. This will be displayed as a comment above all of the settings regardless what was inputted
+     */
     private static final String HEADER = "Main language file for Reach.";
     public static String COMMAND_NO_PERMISSION;
     public static String DISABLED_COMMAND;
     public static String PLAYER_COMMAND;
 
     /**
-     * Apply the language options
+     * This method is utilized from applying all the language settings. This allows us to grab any of these static variables and apply them
+     * throughout the rest of the plugin.
      */
     private static void init() {
         COMMAND_NO_PERMISSION = config.getString("command.no-permission", "&4You do not have permission to use {getCommand}!");
@@ -31,14 +35,27 @@ public class Lang {
 
 
     // ############################  DO NOT EDIT BELOW THIS LINE  ############################
-
+    /**
+     * Initialize plugin instance. we use this to initialize the config file.
+     */
     private static final Main plugin = Main.getInstance();
+    /**
+     * Language file. Gets the default set language file from the default config file.
+     */
     private static final String langFile = Config.LANGUAGE_FILE;
+    /**
+     * Configuration File. Gets the default plugin folder and creates a new config file from the default provided config file with comments.
+     */
     private static final File configFile = new File(plugin.getDataFolder(), langFile);;
+    /**
+     * Config file configuration. This is used for creating the files settings general speaking.
+     */
     private static FileConfiguration config;
 
     /**
-     * Reload the language file
+     * This method will reload the language file and its contents. If there is no language
+     * file, then a default language file will be generated and then will load the language
+     * file. The comments that are set within the language file will also not be overwritten.
      */
     public static void reload() {
         if (!configFile.exists()) {
